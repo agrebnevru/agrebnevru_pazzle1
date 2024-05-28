e2bffd0d2782b85c8b324c3b36c354177c9b81de
Alex Grebnev <my.grebnev.work@gmail.com>
2024-05-28 18:38:06 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/agrebnevru_pazzle1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agrebnevru_pazzle1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:agrebnevru_pazzle1/box-game.dart';
+import 'package:flame/math.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  BoxGame game = BoxGame();
+  runApp(game.widget);
+}
